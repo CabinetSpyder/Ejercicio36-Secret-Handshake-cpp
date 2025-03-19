4befaad3974082c60faeae439f2c3f8921b28a52
AndresAAV <andres.artaloytia@gmail.com>
2025-03-19 14:30:26 +0100

    Primera iteracion de la solucion

--- secret-handshake/secret_handshake.h
+++ secret-handshake/secret_handshake.h
@@ -1,9 +1,14 @@
 #if !defined(SECRET_HANDSHAKE_H)
 #define SECRET_HANDSHAKE_H
 
+#include <string>
+#include <vector>
+#include <cmath>
+
 namespace secret_handshake {
 
 // TODO: add your solution here
+    std::vector<std::string> commands(int n);
 
 }  // namespace secret_handshake
 
